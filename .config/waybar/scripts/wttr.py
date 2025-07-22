74cf4a767af4a55d01a4fb4e27804377f5fe02c8
Math1313 <mathisgauthier@hotmail.com>
2025-07-22 11:39:00 -0400

    chore: update waybar config

--- .config/waybar/scripts/wttr.py
+++ .config/waybar/scripts/wttr.py
@@ -58,7 +58,7 @@ WEATHER_CODES = {
 data = {}
 
 
-weather = requests.get("https://wttr.in/Springfield, Manitoba?format=j1").json()
+weather = requests.get("https://wttr.in/saint-felicien,canada?format=j1").json()
 
 
 def format_time(time):
